d1db8104432898178a69cd99b917eb22796b6680
相商 <oncwnuLNuclItdHXxQA8u7gv7Dt0@git.weixin.qq.com>
2020-09-12 19:23:33 +0800

    1.Add out of range number

---
+++ chapter2/src/com/github/greekpanda/chapter2/TypeDemo03.scala
@@ -0,0 +1,20 @@
+package com.github.greekpanda.chapter2
+
+/**
+ * @author GreekPanda
+ * @date 2020/9/12 0012 下午 7:19
+ * @version 1.0
+ */
+object TypeDemo03 {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  var i = 10
+  var j = 101
+  //  var e:Long = 92233720368547758071 //error, it's out of range of long
+
+  var num1: Float = 2.2434343431f
+  var num2: Double = 2.33432323121212112
+  println("num1 = " + num1 + "\t" + "num2 = " + num2)
+}
