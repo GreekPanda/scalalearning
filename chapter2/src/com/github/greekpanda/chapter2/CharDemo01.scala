ee514d5b8501a2741d86b80c03e9fd47c4527ffa
相商 <oncwnuLNuclItdHXxQA8u7gv7Dt0@git.weixin.qq.com>
2020-09-12 21:49:36 +0800

    1.Use char type

---
+++ chapter2/src/com/github/greekpanda/chapter2/CharDemo01.scala
@@ -0,0 +1,17 @@
+package com.github.greekpanda.chapter2
+
+/**
+ * @author GreekPanda
+ * @date 2020/9/12 0012 下午 7:24
+ * @version 1.0
+ */
+object CharDemo01 {
+  def main(args: Array[String]): Unit = {
+    val char1:Char = 97
+    println("char1 = " + char1)
+
+    val char2: Char = 'a'
+    val num = 10 + char2
+    println("num = " + num)
+  }
+}
