47627c01170db5993407f1fc0f54d2eefbb514a3
相商 <oncwnuLNuclItdHXxQA8u7gv7Dt0@git.weixin.qq.com>
2020-09-13 18:20:56 +0800

    1.函数练习题

---
+++ chapter5/src/com/github/greekpanda/chapter5/Exercise01.scala
@@ -0,0 +1,22 @@
+package com.github.greekpanda.chapter5
+
+/**
+ * @author GreekPanda
+ * @date 2020/9/13 0013 下午 6:11
+ * @version 1.0
+ */
+object Exercise01 {
+  def main(args: Array[String]): Unit = {
+    def f1 = "hello, world"
+
+    println(f1)
+
+    //    val ret = f1()
+    //    println(ret)
+  }
+
+  //上面中等价于声明一个函数
+  //  def f1() = {
+  //    "hello, world"
+  //  }
+}
